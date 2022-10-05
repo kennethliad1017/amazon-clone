f68eb43fc23003e9dbcdddb3eb84f8aa9ed8777d
Kenneth Liad <kennethnungay.liad@gmail.com>
2022-10-05 19:07:20 +0800

    Added routes to pages in App.tsx and update App.css to body for background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./layouts";
+import Cart from "./pages/Cart.page";
 import Home from "./pages/Home.page";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
